fa8d418c749fdb74a590d49cf8b2b5387c22fba7
CodeSolutions11 <codesolutions11ca@gmail.com>
2025-07-02 03:06:54 -0400

    Clase #19 - 01/07/2025

---
+++ js/julio/clase19_0107.js
@@ -0,0 +1,83 @@
+// TODO: Operadores de comparación
+// * 1.Operador de igualdad ==
+
+// let passwd = "123456"
+// let resultado = "123456" == passwd
+
+// document.write(resultado)
+
+// * 2.Operador de desigualdad !=
+
+// let name1 = "Kelvin"
+// let name2 = "keinny"
+
+// let resultado = name1 != "Kelvin"
+// document.writeln(resultado)
+
+// * 3.Operador de mayor que >
+
+// let resultado = 16 > 16;
+// document.writeln(resultado);
+
+// * 4.Operador de mayor o igual  >=
+
+// let resultado = 30 >= 20
+// document.writeln(resultado)
+
+// * 5.Operador de menor que  <
+// document.writeln(18 < 3)
+
+// * 6.Operador de menor que  <=
+// document.writeln(20<=14)
+
+// * 7. Operador de estrictamente igual ===
+// document.writeln("2" === 2)
+
+// * 8. Operador de estrictamente diferente !==
+
+// let resultado = 125 !== "125"
+
+// document.writeln(resultado)
+
+// TODO: Operadores de lógicos
+
+// * 1. Operador lógico AND => && . Evalúa 2 o más expresiones  y TODAS devuelve true entonces devuelve true pero si alguna de las expresiones devuelva false para que la respuesta sea false
+
+// * simulando datos de una bd
+/*
+let user = "jesusDev01";
+let passwd = "123456";
+
+let user_prompt = prompt("Ingresar Usuario");
+let passwd_prompt = prompt("Ingresar contraseña");
+
+console.log(user_prompt);
+console.log(passwd_prompt);
+
+let resultado = user === user_prompt && passwd === passwd_prompt;
+
+document.writeln(resultado);
+*/
+
+// * 2. Operador lógico OR => || .
+
+// let resultado = true || false
+
+// document.writeln(resultado)
+
+// * 3. Operador lógico de negacion NOT => !
+
+// let resultado = 28<5
+
+// document.writeln(!resultado)
+
+// TODO: Condicional if/else
+let num1 = 15;
+let num2 = 30;
+
+if (false) {
+  document.writeln("La expresión es verdadera");
+} else {
+  document.writeln("La expresión es false");   
+}
+
