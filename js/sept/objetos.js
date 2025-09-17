a1056d6d9af033603599d8add97a1be1413373a8
CodeSolutions11 <codesolutions11ca@gmail.com>
2025-09-16 22:32:28 -0400

    Clase #38 - 16/09/2025

---
+++ js/sept/objetos.js
@@ -0,0 +1,22 @@
+const date = "2025/09/16" 
+console.log(date)
+
+const fecha = new Date(date)
+console.log(fecha)
+
+console.log(new Date())
+
+const user = {}
+
+const player = new Object()
+
+console.log(user, player)
+
+class Animal {
+    constructor(raza){
+        this.raza = raza
+    }
+}
+
+const perro = new Animal("perro")
+console.log(perro.raza)
